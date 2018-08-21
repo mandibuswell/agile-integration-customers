800f080bfb1e0ef726f3bff71409866c1f11f1dc
mandibuswell <mandibuswell@users.noreply.github.com>
2018-08-21 22:36:54 +1200

    Update app.js

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use(
 
     connection(mysql,{
 
-        host: '172.30.141.165', // host mysql service
+        host: '172.30.239.152', // host mysql service
         user: 'dbuser',
         password : 'password',
         port : 3306, // port mysql service
